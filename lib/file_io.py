3c6cc2c603a3f8b920717f0a211058d4974df144
CleverChuk <chubiyke@gmail.com>
2022-02-06 00:08:15 -0500

    implemented glcoud file io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,6 +1,9 @@
 from io import BytesIO
+import os
 
 from matplotlib.pyplot import bar
+
+from google.cloud import storage
 
 MAX_CONTENT_LENGTH = 1000 * 1000 * 1000
 
@@ -20,7 +23,7 @@ class FileIO:
 class EnvironmentFileIOFactory(AbstractFileIOFactory):
     def create(self, env: str) -> FileIO:
         if env == "production":
-            return GCloudFileIO()
+            return GCloudFileIO(os.getenv("GCLOUD_BUCKET"))
         return LocalFileIO()
 
 
@@ -34,12 +37,33 @@ class LocalFileIO(FileIO):
     def write(self, bytes: BytesIO, path: str) -> str:
         with open(path, "wb") as fp:
             fp.write(bytes.getvalue())
-            
+
         return "files/export.xlsx"
 
 
 class GCloudFileIO(FileIO):
-    pass
+    def __init__(self, bucket_name) -> None:
+        self.bucket_name = bucket_name
+        self.storage_client = storage.Client()
+        
+    def read(self, path: str) -> BytesIO:
+        """Downloads a blob into memory."""
+        # Get GCS bucket
+        bucket = self.storage_client.bucket(self.bucket_name)
+
+        # Construct a client side representation of a blob.
+        blob = bucket.blob(path)
+        contents: str = blob.download_as_string()
+        return BytesIO(contents.encode("utf-8"))
+
+    def write(self, bytes: BytesIO, path: str) -> str:
+        """Uploads a file to the bucket."""
+        bucket = self.storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(path)
+        blob.upload_from_file(bytes)
+
+        return blob.public_url
+
 
 
 
